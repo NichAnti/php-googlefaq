998d4877e472fcae40e330fb2992bdea5d910f8a
Nicholas <nicholas.zx.99@gmail.com>
2019-04-19 17:25:42 +0200

    ok

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>faq</title>
+  </head>
+  <body>
+    <nav>
+      <div class="logo-title">
+        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png" alt="nav-logo">
+        <h1>Privacy e termini</h1>
+      </div>
+      <div class="menu">
+        <span>Introduzione</span>
+        <span>Norme sulla privacy</span>
+        <span>Termini di servizio</span>
+        <span>Tecnologie</span>
+        <span>Domande frequenti</span>
+      </div>
+    </nav>
+  </body>
+</html>
